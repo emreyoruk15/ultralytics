7394ae08e60fedb608266dda66c0db328195d370
emreyoruk15 <emryoruk15@gmail.com>
2025-08-19 16:32:31 +0300

    PConv class added

--- ultralytics/nn/modules/conv.py
+++ ultralytics/nn/modules/conv.py
@@ -23,6 +23,7 @@ __all__ = (
     "Concat",
     "RepConv",
     "Index",
+    "PConv"
 )
 
 
@@ -90,6 +91,27 @@ class Conv(nn.Module):
             (torch.Tensor): Output tensor.
         """
         return self.act(self.conv(x))
+
+
+class PConv(nn.Module):  
+    ''' Pinwheel-shaped Convolution using the Asymmetric Padding method. '''
+    
+    def __init__(self, c1, c2, k, s):
+        super().__init__()
+
+        # self.k = k
+        p = [(k, 0, 1, 0), (0, k, 0, 1), (0, 1, k, 0), (1, 0, 0, k)]
+        self.pad = [nn.ZeroPad2d(padding=(p[g])) for g in range(4)]
+        self.cw = Conv(c1, c2 // 4, (1, k), s=s, p=0)
+        self.ch = Conv(c1, c2 // 4, (k, 1), s=s, p=0)
+        self.cat = Conv(c2, c2, 2, s=1, p=0)
+
+    def forward(self, x):
+        yw0 = self.cw(self.pad[0](x))
+        yw1 = self.cw(self.pad[1](x))
+        yh0 = self.ch(self.pad[2](x))
+        yh1 = self.ch(self.pad[3](x))
+        return self.cat(torch.cat([yw0, yw1, yh0, yh1], dim=1))
 
 
 class Conv2(Conv):
